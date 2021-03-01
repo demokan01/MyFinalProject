96d9669f189c969e8c4fe5a2f7a0c2702544b375
demokan01 <dmkn01@hotmail.com>
2021-03-01 23:21:30 +0300

    updated

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,15 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,22 +19,26 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        
 
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
+            
         }
 
+
+        [ValidationAspect(typeof (ProductValidator))]
         public IResult Add(Product product)
         {
-            //magic strings
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-
-            }
+            //business codes
             _productDal.Add(product);
+
             return new SuccessResult(Messages.ProductAdded);
+          
+
+
+            
         }
 
         public IDataResult<List<Product>> GetAll()
---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool//static bir sınıfın methodlarıda static olmalı
+    {
+        public static void Validate(IValidator validator,object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
