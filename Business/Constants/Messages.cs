33e718e714eb51ef32ff3e9b5679bcb7742ffd97
demokan01 <dmkn01@hotmail.com>
2021-03-05 13:35:21 +0300

    updated

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,13 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError="Ürün Limitini Aştınız";
         public static string ProductNameAlreadyExists="Ürün Adı Zaten Var";
         public static string CategoryLimitExceded="Kategori Limiti aşıldı";
+        public static string AuthorizationDenied="Yetkiniz yok";
+        public static string UserRegistered="Kayıt Olundu";
+        public static string UserNotFound="Kullanıcı Bulunamadı";
+        public static string PasswordError="Şifre Yanlış";
+        public static string SuccessfulLogin="Giriş Başarılı";
+        public static string UserAlreadyExists="ku";
+        public static string AccessTokenCreated="Token üretildi";
+        
     }
 }
